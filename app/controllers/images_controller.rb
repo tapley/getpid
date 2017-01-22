c0541651a6897c89a9ad72478ab817a0c9e25179
Tapley Stephenson <tapley.stephenson@gmail.com>
2017-01-21 20:27:43 -0500

    update controller responses

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,12 @@
 class ImagesController < ApplicationController
 
   def show
-    render Patient.find_by_image(secure_params.image), format: :json
+    patient = Patient.find_by_image(secure_params.image)
+    if patient
+      render :json => { patient: patient }
+    else
+      render :json => { :errors => "Patient not found.", :status => 404 }
+    end
   end
 
   private
